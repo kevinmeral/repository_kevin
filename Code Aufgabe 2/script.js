6b1086c21089e0f3b26bbd9c7166c92456b430db
kevinmeral <kevin.enes.meral@hs-furtwangen.de>
2021-11-28 21:52:54 +0100

    Update script.js

--- Code Aufgabe 2/script.js
+++ Code Aufgabe 2/script.js
@@ -1,48 +1,47 @@
+"use strict";
 var script;
 (function (script) {
-    var table = document.getElementById("Tabelle");
-    var tagesausflugInput = (document.getElementById("Tagesausflug"));
-    var preisInput = (document.getElementById("Preis"));
-    var datumInput = (document.getElementById("Datum"));
-    var submit = (document.getElementById("submit"));
-    var clear = (document.getElementById("clear"));
-    var rows = [];
-    var loadRows = [];
-    var savedRows;
-    window.addEventListener("load", function () {
+    const table = document.getElementById("Tabelle");
+    const tagesausflugInput = (document.getElementById("Tagesausflug"));
+    const preisInput = (document.getElementById("Preis"));
+    const datumInput = (document.getElementById("Datum"));
+    const submit = (document.getElementById("submit"));
+    const clear = (document.getElementById("clear"));
+    let rows = [];
+    let loadRows = [];
+    let savedRows;
+    window.addEventListener("load", () => {
         loadTable();
     });
-    clear.addEventListener("click", function () {
-        localStorage.clear();
-    });
-    submit.addEventListener("click", function () {
+    submit.addEventListener("click", () => {
+        console.log("test");
         createEvent(tagesausflugInput.value, preisInput.value, datumInput.value.substring(0, 10), datumInput.value.substring(11, 16), true);
         setTimeout(function () {
             clearInput();
         }, 100);
     });
     function createEvent(tagesausflugText, preisText, datumText, tageszeitText, save) {
-        var tabellenEintrag = document.createElement("tr");
-        var tagesausflug = document.createElement("td");
+        let tabellenEintrag = document.createElement("tr");
+        let tagesausflug = document.createElement("td");
         tagesausflug.textContent = tagesausflugText;
-        var preis = document.createElement("td");
+        let preis = document.createElement("td");
         preis.textContent = preisText;
-        var datum = document.createElement("td");
+        let datum = document.createElement("td");
         datum.textContent = datumText;
-        var tageszeit = document.createElement("td");
+        let tageszeit = document.createElement("td");
         tageszeit.textContent = tageszeitText;
-        var löschZeile = document.createElement("td");
-        var löschButton = document.createElement("i");
-        löschButton.className = "fa fa-trash";
+        let löschZeile = document.createElement("td");
+        let löschButton = document.createElement("button");
+        löschButton.innerHTML = "Löschen";
         table.appendChild(tabellenEintrag);
         tabellenEintrag.appendChild(tagesausflug);
+        tabellenEintrag.appendChild(preis);
         tabellenEintrag.appendChild(datum);
         tabellenEintrag.appendChild(tageszeit);
-        tabellenEintrag.appendChild(preis);
-        tabellenEintrag.appendChild(löschButton);
+        tabellenEintrag.appendChild(löschZeile);
         löschZeile.appendChild(löschButton);
         if (save) {
-            var saveRow = {
+            let saveRow = {
                 tagesausflug: tabellenEintrag.textContent,
                 preis: preis.textContent,
                 datum: datum.textContent,
@@ -53,7 +52,7 @@ var script;
             console.log(savedRows);
             localStorage.setItem("savedRows", savedRows);
         }
-        löschButton.addEventListener("click", function () {
+        löschButton.addEventListener("click", () => {
             table.removeChild(tabellenEintrag);
         });
     }
@@ -67,7 +66,7 @@ var script;
             return;
         loadRows = JSON.parse(localStorage.getItem("savedRows"));
         console.log(loadRows);
-        for (var i = 0; i < loadRows.length; i++) {
+        for (let i = 0; i < loadRows.length; i++) {
             createEvent(loadRows[i].tagesausflug, loadRows[i].preis, loadRows[i].datum, loadRows[i].tageszeit, false);
         }
         rows = loadRows;
