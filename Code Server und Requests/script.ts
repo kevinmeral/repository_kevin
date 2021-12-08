f4d5096d95782919aaea8ba0440b52229ad81712
kevinmeral <92145149+kevinmeral@users.noreply.github.com>
2021-12-08 18:00:37 +0100

    Update script.ts

--- Code Server und Requests/script.ts
+++ Code Server und Requests/script.ts
@@ -1,103 +1,35 @@
-namespace script {
+namespace Client {
+    console.log("Client läuft"); //Testausgabe
 
+    const url: string = "http://127.0.0.1:3000"; 
+    const path: string = "/greetings"; 
 
-  const table: HTMLElement = document.getElementById("Tabelle");
-  const tagesausflugInput: HTMLInputElement = <HTMLInputElement>(document.getElementById("Tagesausflug"));
-  const preisInput: HTMLInputElement = <HTMLInputElement>(document.getElementById("Preis"));
-  const datumInput: HTMLInputElement = <HTMLInputElement>(document.getElementById("Datum"));
-  const submit: HTMLButtonElement = <HTMLButtonElement>(document.getElementById("submit"));
-  const clear: HTMLButtonElement = <HTMLButtonElement>(document.getElementById("clear"));
+    const myForm: HTMLFormElement = <HTMLFormElement>document.getElementById("myform");
+    const sendButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send-button"); 
 
-  interface TabellenEintrag {
-        tagesausflug: string;
-        preis: string;
-        datum: string;
-        tageszeit: string;
-    }
+    sendButton.addEventListener("click", function(evt: Event){
+        evt.preventDefault(); 
+        sendForm(); 
 
-  let rows: TabellenEintrag[] = [];    
-  let loadRows: TabellenEintrag[] = [];
-  let savedRows: string;
-
-
-  window.addEventListener("load", (): void => {
-        loadTable();
     });
 
+    console.log(myForm, sendButton);
 
-  
 
-  submit.addEventListener("click", (): void => {
-    console.log("test");
-    createEvent(tagesausflugInput.value, preisInput.value, datumInput.value.substring(0, 10), datumInput.value.substring(11, 16), true);
-    setTimeout(function(): void {
-        clearInput();
-    },         100);
-  });
-   
-    function createEvent(tagesausflugText: string, preisText: string, datumText: string, tageszeitText: string, save: boolean): void {
-        let tabellenEintrag: HTMLElement = document.createElement("tr");
-        let tagesausflug: HTMLElement = document.createElement("td");
-        tagesausflug.textContent = tagesausflugText;
-        let preis: HTMLElement = document.createElement("td");
-        preis.textContent = preisText;
-        let datum: HTMLElement = document.createElement("td");
-        datum.textContent = datumText;
-        let tageszeit: HTMLElement = document.createElement("td");
-        tageszeit.textContent = tageszeitText;
-        let löschZeile: HTMLElement = document.createElement("td");
-        let löschButton: HTMLButtonElement = <HTMLButtonElement>document.createElement("button");
-        löschButton.innerHTML = "Löschen";
-        
-        table.appendChild(tabellenEintrag);
-        tabellenEintrag.appendChild(tagesausflug);
-        tabellenEintrag.appendChild(preis);
-        tabellenEintrag.appendChild(datum);
-        tabellenEintrag.appendChild(tageszeit);
-        tabellenEintrag.appendChild(löschZeile);
-        löschZeile.appendChild(löschButton);
-  
-        
-        if (save) {
-            let saveRow: TabellenEintrag = {
-                tagesausflug: tabellenEintrag.textContent,
-                preis: preis.textContent,
-                datum: datum.textContent,
-                tageszeit: tageszeit.textContent
-            };
-            rows.push(saveRow);
-    
-            savedRows = JSON.stringify(rows);
-            console.log(savedRows);
-            localStorage.setItem("savedRows", savedRows);
-        }
-  
-        löschButton.addEventListener("click", (): void => {
-            table.removeChild(tabellenEintrag);
-        });  
+  async function sendForm(): Promise<void> {
 
-}
 
-  function clearInput(): void {
-      tagesausflugInput.value = "";
-      preisInput.value = "";
-      datumInput.value = "";    
+
+        let formData: FormData= new FormData(myForm); 
+        let query: URLSearchParams = new URLSearchParams(<any>formData); 
+        let urlWithQuery: string = url + path + "?" + query.toString(); 
+
+
+
+        let response: Response = await fetch(urlWithQuery); 
+        let responseText: string = await response.text(); 
+        console.log(responseText); 
+
   }
 
-
-  function loadTable(): void {
-      if (localStorage.length < 1)
-          return;
-
-      loadRows = JSON.parse(localStorage.getItem("savedRows"));
-      console.log(loadRows);
-      for (let i: number = 0; i < loadRows.length; i++) {
-          createEvent(loadRows[i].tagesausflug, loadRows[i].preis, loadRows[i].datum, loadRows[i].tageszeit, false);
-      }
-      rows = loadRows;
-      loadRows = [];
-  }
-
-
-  
 }
